2f843e94a7ca1208bb996689c19956d557f7ba22
Alla Rudenko <whitney2206@gmail.com>
2020-08-18 22:00:22 -0400

    static

--- day43_Static_Continue/Test.java
+++ day43_Static_Continue/Test.java
@@ -1,7 +1,7 @@
 package day43_Static_Continue;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-import org.w3c.dom.ls.LSOutput;
+import java.util.Arrays;
+import  static Library.Resources.*;
 
 public class Test {
 
@@ -10,6 +10,15 @@ public class Test {
         System.out.println(Static_Block_3.a);       // 100
         System.out.println(Static_Block_3.b);       // 200
         System.out.println(Static_Block_3.c);       // 300
+
+
+        System.out.println(Arrays.toString(Data.arr));
+        System.out.println(Data.list);
+
+        System.out.println("======================================================");
+
+        System.out.println(a);
+        method1();
     }
 
 }
